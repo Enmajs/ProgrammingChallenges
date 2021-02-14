67c0446f90b2e810cfabf3469b4dffe9a030a5e5
Enma <enmakk>
2021-02-13 20:41:09 -0500

    Binary search

---
+++ ProgrammingChallengesCsharp/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿
+namespace ProgrammingChallengesCsharp
+{
+    public static class BinarySearch
+    {
+        public static object BinarySearchIterative(int[] arrayNumbers, int target)
+        {
+            int minValue = 0;
+            int maxValue = arrayNumbers.Length - 1;
+            while (minValue <= maxValue)
+            {
+                int midElement = (minValue + maxValue) / 2;
+                if (target == arrayNumbers[midElement])
+                {
+                    return ++midElement;
+                }
+                else if (target < arrayNumbers[midElement])
+                {
+                    maxValue = midElement - 1;
+                }
+                else
+                {
+                    minValue = midElement + 1;
+                }
+            }
+            return "Nil";
+        }
+    }
+}
---
+++ ProgrammingChallengesCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingChallengesCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2, 5, 3, 65 };
+            BinarySearch.BinarySearchIterative(numbers, 9);
+        }
+    }
+
+
+}
