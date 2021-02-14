299354a5098c090b99bb7b4b83face8df7cebf93
Enma <enmakk>
2021-02-14 11:48:07 -0500

    bubble sort

---
+++ ProgrammingChallengesCsharp/MergeSortedArrays.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrammingChallengesCsharp
+{
+    public class MergeSortedArrays
+    {
+
+        public static void BubbleSort()
+        {
+            int[] array = { 232, 9, 1, 209, 4, 99, 20, 22, 11 };
+
+            int temp;
+
+            for (int j = 0; j <= array.Length - 2; j++)
+            {
+                for (int i = 0; i <= array.Length -2; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
+            System.Console.WriteLine(String.Join("\n", array));
+
+            Console.ReadLine();
+        }
+
+    }
+}
--- ProgrammingChallengesCsharp/Program.cs
+++ ProgrammingChallengesCsharp/Program.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProgrammingChallengesCsharp
+﻿namespace ProgrammingChallengesCsharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 2, 5, 3, 65 };
-            BinarySearch.BinarySearchIterative(numbers, 9);
+            MergeSortedArrays.BubbleSort();
         }
     }
 
